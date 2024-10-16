e87e6a4f6b686cddecc45a1578ee2f3fa688b648
Vicky Singh <vickysingh88050@gmail.com>
2024-10-16 19:32:55 +0530

    Added Docker Container

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,28 @@
 require("dotenv").config();
-const express=require('express');
-const mongoose=require('mongoose')
-const cors=require('cors')
-require('./models/model')
-require('./models/post')
-const app=express();
-const port=5000;
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("./models/model");
+require("./models/post");
+const app = express();
+const port = 5000;
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use(require("./routes/auth"));
 app.use(require("./routes/createpost"));
 app.use(require("./routes/user"));
 
+console.log(process.env.MONGODB_URI);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("not connected to database");
+  });
 
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
-  console.log("connected to database")
-}).catch(()=>{
-  console.log("not connected to database")
-})
-
-
-app.listen(port,()=>{
-    console.log("server is running on port 5000")
-})
+app.listen(port, () => {
+  console.log("server is running on port 5000");
+});
